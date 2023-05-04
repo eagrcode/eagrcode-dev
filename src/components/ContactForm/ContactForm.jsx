d2d2b475472d566a45673489e7d530a4866227e4
eagrcode <elliot.robinson92@hotmail.com>
2023-05-04 11:29:01 +0100

    add contact form + additional styling

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -18,17 +18,26 @@ export default function ContactForm() {
       action="https://formspree.io/f/myyaojra"
       method="POST"
     >
-      <label htmlFor="firstName">First name</label>
-      <input id="firstName" type="text" name="firstName" />
-      <label htmlFor="lastName">Last name</label>
-      <input id="lastName" type="text" name="lastName" />
-      <label htmlFor="email">Email Address</label>
-      <input id="email" type="email" name="email" />
-      <ValidationError prefix="Email" field="email" errors={state.errors} />
-      <label htmlFor="message">Message</label>
-      <textarea id="message" name="message" />
-      <ValidationError prefix="Message" field="message" errors={state.errors} />
-      <button type="submit" disabled={state.submitting}>
+      <div>
+        <label htmlFor="firstName">First name</label>
+        <input className={styles.input} id="firstName" type="text" name="First name" />
+      </div>
+      <div>
+        <label htmlFor="lastName">Last name</label>
+        <input className={styles.input} id="lastName" type="text" name="Last name" />
+      </div>
+      <div>
+        <label htmlFor="email">Email Address</label>
+        <input className={styles.input} id="email" type="email" name="Email" />
+        <ValidationError prefix="Email" field="email" errors={state.errors} />
+      </div>
+      <div>
+        <label htmlFor="message">Message</label>
+        <textarea className={styles.message} id="message" name="Message" />
+        <ValidationError prefix="Message" field="message" errors={state.errors} />
+      </div>
+
+      <button className={styles.button} type="submit" disabled={state.submitting}>
         Submit
       </button>
     </form>
