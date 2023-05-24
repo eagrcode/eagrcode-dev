874afc949f02046285d508ce5c70159ee440a332
eagrcode <elliot.robinson92@hotmail.com>
2023-05-24 10:47:44 +0100

    add functionality to close mobile nav menu when clicking main or on touch start

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -6,15 +6,23 @@ import styles from "./Layout.module.scss";
 
 // context
 import { useTheme } from "@/context/ThemeProvider";
+import { useContextProvider } from "@/context/ContextProvider";
 
 export default function NavLayout({ children }: { children: React.ReactNode }) {
   // initial state
   const { theme } = useTheme();
+  const { setIsOpen, isOpen } = useContextProvider();
 
   return (
     <div className={styles.wrapper}>
       <Navbar />
-      <main className={`${styles.main} ${styles[theme]}`}>{children}</main>
+      <main
+        onTouchStart={() => setIsOpen(false)}
+        onClick={() => setIsOpen(false)}
+        className={`${styles.main} ${styles[theme]}`}
+      >
+        {children}
+      </main>
     </div>
   );
 }
