ed2e4aad4ad1320ffe330c36f59d0d666e5ebb90
eagrcode <elliot.robinson92@hotmail.com>
2023-05-31 13:10:34 +0100

    remove mobile swipe state change

--- src/components/Projects/ProjectCard/Swiper/Swiper.tsx
+++ src/components/Projects/ProjectCard/Swiper/Swiper.tsx
@@ -4,31 +4,28 @@ import styles from "./Swiper.module.scss";
 // next
 import Image, { StaticImageData } from "next/image";
 
+// react
+import { useState, TouchEvent, Dispatch, SetStateAction } from "react";
+
 // prop types
-type props = {
+type Props = {
   images: {
-    "1": StaticImageData;
-    "2": StaticImageData;
-    "3": StaticImageData;
+    [key: string]: StaticImageData;
   };
   alt: string;
   activeIndex: number;
 };
 
-export default function Swiper({ images, alt, activeIndex }: props) {
+export default function Swiper({ images, alt, activeIndex }: Props) {
   return (
     <div className={styles.swiper}>
       <div
         className={styles.swiperInner}
-        style={{ transform: `translateX(-${activeIndex * 100}%)` }}
+        style={{ transform: `translate(-${activeIndex * 100}%)` }}
       >
-        {Object.keys(images).map((image, index) => (
-          <div key={index} className={styles.img}>
-            <Image
-              alt={alt}
-              src={images[image as keyof typeof images]}
-              style={{ height: "auto", width: "100%" }}
-            />
+        {Object.keys(images).map((key) => (
+          <div key={key} className={styles.img}>
+            <Image alt={alt} src={images[key]} style={{ height: "auto", width: "100%" }} />
           </div>
         ))}
       </div>
