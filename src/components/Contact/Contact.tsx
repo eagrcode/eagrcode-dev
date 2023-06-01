30d45f109dc27e20458a83780ceaa100383badb7
eagrcode <elliot.robinson92@hotmail.com>
2023-06-01 12:40:09 +0100

    restructure Contact layout

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -11,6 +11,9 @@ import ContactForm from "./ContactForm/ContactForm";
 import { useTheme } from "@/context/ThemeProvider";
 import { useContextProvider } from "@/context/ContextProvider";
 
+// components
+import IconContainer from "../IconContainer/IconContainer";
+
 export default function Contact() {
   // initial state
   const { theme } = useTheme();
@@ -19,25 +22,28 @@ export default function Contact() {
   return (
     <section className={styles.contactSection}>
       <div className={isOpen ? `${styles.contactWrapper} ${styles.isOpen}` : styles.contactWrapper}>
-        <div className={styles.textContainer}>
-          <h2 id="scroll-to-contact" className={theme}>
-            Contact Me
-          </h2>
-          <p className={theme}>
-            I'm currently open for work, if you wish to get in touch please use the form below.
-          </p>
-          <p className={theme}>
-            Alternatively, you can contact me direct via{" "}
-            <Link
-              className={`${styles.emailLink} ${styles[theme]}`}
-              href="mailto:elliot.robinson92@hotmail.com"
-              title="Open email app"
-            >
-              elliot.robinson92@hotmail.com
-            </Link>
-          </p>
+        <h2 id="scroll-to-contact" className={theme}>
+          Contact Me
+        </h2>
+        <div className={styles.contentContainer}>
+          <div className={styles.textContainer}>
+            <p className={theme}>
+              I'm currently open for work, if you wish to get in touch please use the form below.
+            </p>
+            <p className={theme}>
+              Alternatively, you can contact me direct via{" "}
+              <Link
+                className={`${styles.emailLink} ${styles[theme]}`}
+                href="mailto:elliot.robinson92@hotmail.com"
+                title="Open email app"
+              >
+                elliot.robinson92@hotmail.com
+              </Link>
+            </p>
+            <IconContainer />
+          </div>
+          <ContactForm />
         </div>
-        <ContactForm />
       </div>
     </section>
   );
